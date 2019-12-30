53bcdaac48bc3bc94e9d52bf37e92f6cccafbe44
antonc27 sw <antonc27@mail.ru>
2019-12-30 23:23:02 +0100

    Find vertical and horizontal seams

--- week2/seam/SeamCarver.java
+++ week2/seam/SeamCarver.java
@@ -6,7 +6,7 @@
 
 import edu.princeton.cs.algs4.Picture;
 
-import java.awt.Color;
+import java.util.Arrays;
 
 public class SeamCarver {
 
@@ -69,12 +69,82 @@ public class SeamCarver {
 
     // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
-        return new int[1];
+        transpose();
+        int[] seam = findVerticalSeam();
+        transpose();
+        return seam;
+    }
+
+    private void transpose() {
+        int w = width();
+        int h = height();
+
+        Picture tmp = new Picture(h, w);
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
+                tmp.setRGB(j, i, picture.getRGB(i, j));
+            }
+        }
+
+        picture = tmp;
     }
 
     // sequence of indices for vertical seam
     public int[] findVerticalSeam() {
-        return new int[1];
+        int w = width();
+        int h = height();
+
+        double[][] distTo = new double[w][h];
+        for (int i = 0; i < w; i++) {
+            distTo[i][0] = 1000.0;
+            for (int j = 1; j < h; j++) {
+                distTo[i][j] = Double.POSITIVE_INFINITY;
+            }
+        }
+
+        int[][] edgeTo = new int[w][h];
+        for (int i = 0; i < w; i++) {
+            edgeTo[i][0] = -1;
+        }
+
+        double[][] energy = new double[w][h];
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < h; j++) {
+                energy[i][j] = energy(i, j);
+            }
+        }
+
+        for (int y = 0; y < h-1; y++) {
+            for (int x = 0; x < w; x++) {
+                for (int k = -1; k <= 1; k++) {
+                    int t = x+k;
+                    if (t < 0 || t >= w) continue;
+
+                    if (distTo[t][y + 1] > distTo[x][y] + energy[t][y + 1]) {
+                        distTo[t][y + 1] = distTo[x][y] + energy[t][y + 1];
+                        edgeTo[t][y + 1] = x;
+                    }
+                }
+            }
+        }
+
+        int minPathIdx = -1;
+        double minPathVal = Double.POSITIVE_INFINITY;
+
+        for (int i = 0; i < w; i++) {
+            if (distTo[i][h - 1] < minPathVal) {
+                minPathVal = distTo[i][h - 1];
+                minPathIdx = i;
+            }
+        }
+
+        int[] seam = new int[h];
+        for (int j = h-1; j >= 0; j--) {
+            seam[j] = minPathIdx;
+            minPathIdx = edgeTo[minPathIdx][j];
+        }
+
+        return seam;
     }
 
     // remove horizontal seam from current picture
@@ -95,29 +165,19 @@ public class SeamCarver {
 
     //  unit testing (optional)
     public static void main(String[] args) {
-        Picture testPicture = new Picture(3, 4);
-        testPicture.set(0, 0, new Color(255, 101, 51));
-        testPicture.set(1, 0, new Color(255, 101, 153));
-        testPicture.set(2, 0, new Color(255, 101, 255));
+        Picture testPicture = new Picture("3x4.png");
+        SeamCarver sc1 = new SeamCarver(testPicture);
 
-        testPicture.set(0, 1, new Color(255, 153, 51));
-        testPicture.set(1, 1, new Color(255, 153, 153));
-        testPicture.set(2, 1, new Color(255, 153, 255));
+        assertTrue(sc1.height() == 4, "SeamCarver height");
+        assertTrue(sc1.width() == 3, "SeamCarver width");
 
-        testPicture.set(0, 2, new Color(255, 203, 51));
-        testPicture.set(1, 2, new Color(255, 204, 153));
-        testPicture.set(2, 2, new Color(255, 205, 255));
+        assertTrue(sc1.energy(0, 0) == 1000.0, "Energy of (0, 0)");
+        assertTrue(Math.pow(sc1.energy(1, 2), 2) - 52024.0 < 0.001, "Energy of (1, 2)");
 
-        testPicture.set(0, 3, new Color(255, 255, 51));
-        testPicture.set(1, 3, new Color(255, 255, 153));
-        testPicture.set(2, 3, new Color(255, 255, 255));
+        Picture testPicture2 = new Picture("6x5.png");
+        SeamCarver sc2 = new SeamCarver(testPicture2);
 
-        SeamCarver sc = new SeamCarver(testPicture);
-
-        assertTrue(sc.height() == 4, "SeamCarver height");
-        assertTrue(sc.width() == 3, "SeamCarver width");
-
-        assertTrue(sc.energy(0, 0) == 1000.0, "Energy of (0, 0)");
-        assertTrue(Math.pow(sc.energy(1, 2), 2) - 52024.0 < 0.001, "Energy of (1, 2)");
+        assertTrue(Arrays.equals(sc2.findVerticalSeam(), new int[] { 3, 4, 3, 2, 1 }), "Vertical seam");
+        assertTrue(Arrays.equals(sc2.findHorizontalSeam(), new int[] { 1, 2, 1, 2, 1, 0 }), "Horizontal seam");
     }
 }
