2d23b5daed35ed1136d6cf2ad152ac1bfcf40bb0
antonc27 sw <antonc27@mail.ru>
2020-01-25 00:34:56 +0100

    Dummy BoggleSolver

---
+++ week4/boggle/BoggleSolver.java
@@ -0,0 +1,27 @@
+/* *****************************************************************************
+ *  Name: BoggleSolver
+ *  Date: 25/02/2020
+ *  Description: Finds all valid words in a given Boggle board, using a given dictionary.
+ **************************************************************************** */
+
+import java.util.ArrayList;
+
+public class BoggleSolver
+{
+    // Initializes the data structure using the given array of strings as the dictionary.
+    // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
+    public BoggleSolver(String[] dictionary) {
+
+    }
+
+    // Returns the set of all valid words in the given Boggle board, as an Iterable.
+    public Iterable<String> getAllValidWords(BoggleBoard board) {
+        return new ArrayList<>();
+    }
+
+    // Returns the score of the given word if it is in the dictionary, zero otherwise.
+    // (You can assume the word contains only the uppercase letters A through Z.)
+    public int scoreOf(String word) {
+        return 0;
+    }
+}
