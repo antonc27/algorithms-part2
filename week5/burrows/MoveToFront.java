f85143ecf1a6db41de824a2b50ed0f6dde01259c
antonc27 sw <antonc27@mail.ru>
2020-02-16 14:02:01 +0100

    MoveToFront implementation

--- week5/burrows/MoveToFront.java
+++ week5/burrows/MoveToFront.java
@@ -7,20 +7,71 @@
  *  and moving that character to the front of the sequence.
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
 public class MoveToFront {
     // apply move-to-front encoding, reading from standard input and writing to standard output
     public static void encode() {
+        int r = 256;
+        char[] seq = new char[r];
+        for (int i = 0; i < r; i++) {
+            seq[i] = (char) (i & 0xff);
+        }
 
+        while (!BinaryStdIn.isEmpty()) {
+            char nextC = BinaryStdIn.readChar();
+
+            int idx = -1;
+            for (int i = 0; i < r; i++) {
+                if (seq[i] == nextC) {
+                    idx = i;
+                    break;
+                }
+            }
+
+            BinaryStdOut.write((char) (idx & 0xff));
+
+            for (int i = idx; i > 0; i--) {
+                char tmp = seq[i];
+                seq[i] = seq[i - 1];
+                seq[i - 1] = tmp;
+            }
+        }
+
+        BinaryStdOut.close();
     }
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
     public static void decode() {
+        int r = 256;
+        char[] seq = new char[r];
+        for (int i = 0; i < r; i++) {
+            seq[i] = (char) (i & 0xff);
+        }
 
+        while (!BinaryStdIn.isEmpty()) {
+            int idx = BinaryStdIn.readChar();
+
+            BinaryStdOut.write(seq[idx]);
+
+            for (int i = idx; i > 0; i--) {
+                char tmp = seq[i];
+                seq[i] = seq[i - 1];
+                seq[i - 1] = tmp;
+            }
+        }
+
+        BinaryStdOut.close();
     }
 
     // if args[0] is "-", apply move-to-front encoding
     // if args[0] is "+", apply move-to-front decoding
     public static void main(String[] args) {
-
+        if (args[0].equals("-")) {
+            encode();
+        } else if (args[0].equals("+")) {
+            decode();
+        }
     }
 }
